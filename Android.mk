f401e7b894091a003745bcf764cb48522bfb6f8b
aow1980 <wilkes.allen@gmail.com>
2015-09-03 21:20:38 -0400

    Make compatible with SaberMod ARM mode.
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- Android.mk
+++ Android.mk
@@ -27,7 +27,7 @@ endif
 my_cflags_arm64 := -DPNG_ARM_NEON_OPT=2
 
 # Fix for using GCC compiler instead of clang in SaberMod ARM mode.
-ifneq ($(strip $(ENABLE_SABERMOD_ARM_MODE)),true)
+ifneq ($(strip $(ENABLE_ARM_MODE)),true)
 # BUG: http://llvm.org/PR19472 - SLP vectorization (on ARM at least) crashes
 # when we can't lower a vectorized bswap.
 my_cflags_arm += -fno-slp-vectorize
